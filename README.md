57ead981ca4c20ea65fd9f7b18911cf0494c02bd
zerocho <zerohch0@gmail.com>
2023-10-17 15:26:43 +0900

    Add: socket.io

--- README.md
+++ README.md
@@ -87,6 +87,7 @@ brew install redis
 nest g res users
 nest g res posts
 nest g res hashtags
+nest g res chats
 ```
 RouterModule로 등록하기
 [글로벌로 등록도 가능](https://docs.nestjs.com/faq/global-prefix)
@@ -121,10 +122,30 @@ npm i -D @types/bcrypt @types/passport-local
 npm i @nestjs/swagger
 ```
 
-# 웹소켓 개발
+# 웹소켓 개발(Socket.io)
 ```
 npm i @nestjs/websocket
 ```
+
+- namespace와 room으로 구성됨
+    - namespace는 워크스페이스(ws-워크스페이스명, 예시:ws-sleact)
+    - room은 채널, DM
+- @WebSocketGateway({ namespace: '이름' 또는 정규표현식 })
+- @WebSocketServer(): 서비스에서 의존성주입받아 사용할 소켓 서버 객체
+- @SubscribeMessage(이벤트명): 웹소켓 이벤트리스너
+    - @MessageBody(): 이벤트의 데이터가 의존성주입됨
+- afterInit: 웹소켓 초기화가 끝났을 때
+- handleConnection: 클라이언트와 연결이 맺어졌을 때
+    - @ConnectedSocket(): socket을 의존성주입받을 수 있음
+    - socket.emit으로 이벤트 전송 가능(이렇게 하면 모두에게 이벤트 전송)
+    - socket.nsp: 네임스페이스 객체(socket.nsp.emit 하면 해당 네임스페이스 전체에게 이벤트 전송)
+    - socket.nsp.name: 네임스페이스 이름
+    - socket.id: 소켓의 고유 아이디(이걸 사용해서 1대1 메시지도 보낼 수 있음, socket.to(소켓아이디).emit)
+- handleDisconnect: 클라이언트와 연결이 끊어졌을 때
+
+ChatsModule에 이벤트모듈 넣기
+EventsGateway 넣으면 안 됨!!!
+eventsGateway: EventsGateway로 서비스에서 의존성주입 가능
 
 # 배포
 ## 프론트
---
+++ src/events/events.gateway.ts
@@ -0,0 +1,59 @@
+import {
+  ConnectedSocket,
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { onlineMap } from './onlineMap';
+
+@WebSocketGateway({ namespace: /\/ws-.+/ })
+export class EventsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer() public server: Server;
+
+  @SubscribeMessage('test')
+  handleTest(@MessageBody() data: string) {
+    console.log('test', data);
+  }
+
+  @SubscribeMessage('login')
+  handleLogin(
+    @MessageBody() data: { id: number; channels: number[] },
+    @ConnectedSocket() socket: Socket,
+  ) {
+    const newNamespace = socket.nsp;
+    console.log('login', newNamespace);
+    onlineMap[socket.nsp.name][socket.id] = data.id;
+    newNamespace.emit('onlineList', Object.values(onlineMap[socket.nsp.name]));
+    data.channels.forEach((channel) => {
+      console.log('join', socket.nsp.name, channel);
+      socket.join(`${socket.nsp.name}-${channel}`);
+    });
+  }
+
+  afterInit(server: Server): any {
+    console.log('init');
+  }
+
+  handleConnection(@ConnectedSocket() socket: Socket) {
+    console.log('connected', socket.nsp.name);
+    if (!onlineMap[socket.nsp.name]) {
+      onlineMap[socket.nsp.name] = {};
+    }
+    // broadcast to all clients in the given sub-namespace
+    socket.emit('hello', socket.nsp.name);
+  }
+
+  handleDisconnect(@ConnectedSocket() socket: Socket) {
+    console.log('disconnected', socket.nsp.name);
+    const newNamespace = socket.nsp;
+    delete onlineMap[socket.nsp.name][socket.id];
+    newNamespace.emit('onlineList', Object.values(onlineMap[socket.nsp.name]));
+  }
+}
